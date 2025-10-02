b1e84b54843da7a54ff60156ee756d95122a3921
llucaszr <llcsz.rocha@gmail.com>
2025-10-02 19:32:36 -0300

    Adicionando os arquivos que faltava para finalizar a pratica05.

--- praticas/pratica05/app.js
+++ praticas/pratica05/app.js
@@ -1,16 +1,26 @@
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+
+const tarefaRouter = require("./routes/tarefasRouter");
+
 const app = express();
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+
+app.use("/tarefas", tarefaRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 module.exports = app;
--- praticas/pratica05/tests/app.test.js
+++ praticas/pratica05/tests/app.test.js
@@ -4,53 +4,51 @@ const request = supertest(app);
 
 const url = "/tarefas";
 
-describe("Teste da rota/tarefas", () => {
-  let id;
-
-  test("GET /tarefas deve retornar 200", async () => {
+describe("Teste de rotas", () => {
+  let id = "1a2b";
+  test("GET/ 200", async () => {
     const response = await request.get(url);
     expect(response.status).toBe(200);
-    expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body).not.toBeNull();
   });
-
-  test("POST /tarefas deve retornar 201", async () => {
+  test("POST/ 201", async () => {
     const response = await request.post(url).send({
+      id: id,
       nome: "Estudar Node",
       concluida: false,
     });
-
     expect(response.status).toBe(201);
     expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body).not.toBeNull();
-    expect(response.body["id"]).toBeDefined();
-    id = response.body["id"];
-    expect(response.body["nome"]).toMatch("Estudar Node");
-    expect(response.body["concluida"]).toBeFalsy();
   });
-
-  test("GET /id retorna 200", async () => {
+  test("GET/:id 200", async () => {
     const response = await request.get(`${url}/${id}`);
-
     expect(response.status).toBe(200);
     expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body["id"]).toBe(id);
-    expect(response.body["nome"]).toMatch("Estudar Node");
-    expect(response.body["concluida"]).toBeFalsy();
   });
-
-  test("GET /id retornar 404", async () => {
+  test("GET/1 404", async () => {
     const response = await request.get(`${url}/1`);
-
     expect(response.status).toBe(404);
     expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body["msg"]).toBe("Tarefa não encontrada");
   });
-
-  test("PUT /id retorna 200", async () => {
+  test("PUT/:id 200", async () => {
     const response = await request.put(`${url}/${id}`).send({
       nome: "Estudar Node e Express",
       concluida: true,
     });
+    expect(response.status).toBe(200);
+    expect(response.headers["content-type"]).toMatch(/json/);
+  });
+  test("PUT/1 404", async () => {
+    const response = await request.put(`${url}/1`);
+    expect(response.status).toBe(404);
+    expect(response.headers["content-type"]).toMatch(/json/);
+  });
+
+  test("DELETE/:id 204", async () => {
+    const response = await request.delete(`${url}/${id}`);
+    expect(response.status).toBe(204);
+  });
+  test("DELETE/1 404", async () => {
+    const response = await request.delete(`${url}/1`);
+    expect(response.status).toBe(404);
   });
 });
